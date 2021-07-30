438fe51e1f117d3a30c465e977bf9009e3e621a0
Antônio Evaldo <antonio.evaldo349@gmail.com>
2021-07-30 14:45:00 -0300

    Finalizando módulo 2

--- Conta.js
+++ Conta.js
@@ -1,15 +1,40 @@
+import { Cliente } from "./Cliente.js";
+
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
-        this._cliente = cliente;
+        this.cliente = cliente;
         this._agencia = agencia;
     }
 
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
         }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    sacar(valor) {
+        let taxa = 1;
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa) {
+        const valorSacado = taxa * valor;
+
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
     }
 
     depositar(valor) {
@@ -23,5 +48,9 @@ export class Conta {
     transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+    }
+
+    teste() {
+        console.log("Teste na classe Conta")
     }
 }
--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Em vez de inicializar os atributos sem algum valor, na MDN é indicado que você
 * Declarando sempre as propriedades como sendo privadas inicialmente
 * Problemas de duplicação de código
 * Problemas de código extremamente compartilhado
+
+## Módulo 2
+
+* Especializando o código
+* Herdando propriedades e atributos
+* Sobrecarga de métodos
+* Compartilhando código de forma segura
+* Mantendo nosso código na linguagem do cliente
+* Evitando código muito complicado através da herança
+* Palavra-chave `super`
+* Métodos e propriedades privadas e protegidas
+
